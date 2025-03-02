2e9246110c85846296065f618b0b8d725e53b7f9
Davidrg02 <davrodriguezgu@unal.edu.co>
2025-03-02 17:40:35 -0500

    docker port

--- notification_service/app/database.py
+++ notification_service/app/database.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from bson.objectid import ObjectId
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://user:user@usersdb.jza2h.mongodb.net/?retryWrites=true&w=majority&appName=UsersDB")
 client = AsyncIOMotorClient(MONGO_URI)
 db = client.notification_service  # Database name
 
